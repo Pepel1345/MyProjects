d871ecc49b8448df525e82b83760b768995b9340
pepel1345 <Ythfpmthbpf1345>
2019-05-08 20:25:36 +0300

    new

---
+++ SR_SW.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        public double Vvod(TextBox L)
+        {
+            return Convert.ToDouble(L.Text);
+        }
+        public void ris()
+        {
+            double x = Vvod(textBox1);
+            double Resh = Math.Pow(x, x);
+        }
+
+        public void fus ()
+        {
+            StreamWriter tw = new StreamWriter("Text.txt");
+            tw.WriteLine(DateTime.Now);
+            tw.Close();
+            
+        }
+        public void fis()
+        {
+           // StreamReader tr = new StreamReader("Text.txt");
+            textBox2.Text = File.ReadAllText("Text.txt");
+           //tr.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            fus();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            fis();
+        }
+    }
+}
---
+++ get_set.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+
+        class rechka
+        {
+            public string Name { get; set; } = "Tomik";
+            public int Age { get; set; } = 18;
+        }
+
+
+
+
+
+        static void Main(string[] args)
+        {
+
+
+            rechka tolik = new rechka();
+
+            // tolik.Name = "korsh";
+            try
+            {
+                Console.WriteLine("Enter name");
+                tolik.Name = Console.ReadLine();
+                Console.WriteLine("Enter age");
+                tolik.Age = Convert.ToInt32(Console.ReadLine());
+            }
+
+            catch { }
+
+
+
+            finally
+            {
+                Console.WriteLine(tolik.Name);
+                Console.WriteLine(tolik.Age);
+                Console.ReadLine();
+            }
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
